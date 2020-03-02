ca9c6d6f464063fbd95788bd080d6f5e6d53c279
Tiago Antoniazi Del Guerra <tiago.antoniazi@gmail.com>
2020-03-01 22:39:09 -0300

    carga inicial

---
+++ app/Http/Controllers/RouteContoller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RouteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
